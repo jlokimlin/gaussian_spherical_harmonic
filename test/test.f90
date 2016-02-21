b8fbf04cc829a6519719f6a07f9d1adb721d02ae
jlokimlin <jlokimlin@gmail.com>
2016-02-20 21:28:16 -0800

    Replaced automatic arrays with allocatable arrays to improve performance

--- test/test.f90
+++ test/test.f90
@@ -113,19 +113,19 @@ program test
     integer (ip)::&
         nsav1
     integer (ip)::nsav2
-    integer (ip)::nold
-    integer (ip)::nnow
-    integer (ip)::nnew
+    integer (ip)::n_old
+    integer (ip)::n_now
+    integer (ip)::n_new
     real (wp)::lhat
     real (wp)::phlt(361)
     real (wp)::uhat
-    real (wp)::aa
+    real (wp), parameter :: RADIUS_OF_EARTH_IN_METERS = 6.37122e+6_wp
     real (wp)::uzero
     real (wp)::pzero
-    real (wp)::PI
-    real (wp)::HALF_PI
+    real (wp), parameter :: PI = acos( -1.0_wp )
+    real (wp), parameter :: HALF_PI = 0.5_wp * PI
     real (wp)::dtr
-    real (wp)::omega
+    real (wp), parameter :: ROTATION_RATE_OF_EARTH = 7.292e-5_wp
     real (wp)::alphad
     real (wp)::&
         alpha
@@ -143,11 +143,11 @@ program test
     real (wp)::cthclh
     real (wp)::cthslh
     real (wp)::&
-        clh
+        cos_lh
     real (wp)::time
     real (wp)::that
     real (wp)::sin_l
-    real (wp)::slh
+    real (wp)::sin_lh
     real (wp)::evmax
     real (wp)::epmax
     real (wp)::dvmax
@@ -157,8 +157,7 @@ program test
     real (wp)::cos_l
     real (wp)::v2max
     real (wp)::p2max
-    real (wp)::&
-        vmax
+    real (wp)::vmax
     real (wp)::pmax
     type (GaussianSphericalHarmonic):: this
 
@@ -166,23 +165,19 @@ program test
     write( stdout, '(A)') ' '
     write( stdout, '(A)') 'Non-linear steady-state geostropic flow in a shallow water model'
     write( stdout, '(A)') ' '
-    write( stdout, '(A,I11)') 'Triangular trunction number =', ntrunc
+    write( stdout, '(A,I11)') 'Triangular trunction number  = ', ntrunc
     write( stdout, '(A,I11)') 'Number of gaussian latitudes = ', nlat
     write( stdout, '(A)') ' '
 
-    PI = acos( -1.0_wp )
-    HALF_PI = PI/2.0_wp
     dtr = PI/180.0_wp
-    aa = 6.37122e+6
-    omega = 7.292e-5
-    fzero = omega+omega
+    fzero = 2.0_wp * ROTATION_RATE_OF_EARTH
     uzero = 40.0_wp
-    pzero = 2.94e+4
+    pzero = 2.94e+4_wp
     alphad = 60.0_wp
     alpha = dtr*alphad
 
     dt = 300.0_wp
-    MAXIMUM_NUMBER_OF_TIME_ITERATIONS = nint(86400.0_wp * 5.0_wp/dt, kind=ip)
+    MAXIMUM_NUMBER_OF_TIME_ITERATIONS = nint(864.0e+2_wp * 5.0_wp/dt, kind=ip)
     mprint = MAXIMUM_NUMBER_OF_TIME_ITERATIONS/10
 
     !     compute the derivative of the unrotated geopotential
@@ -198,7 +193,7 @@ program test
             sint = sin(theta)
             cost = cos(theta)
             uhat = compute_initial_unrotated_longitudinal_velocity(uzero,HALF_PI-theta)
-            phlt(i) = cfn*cost*uhat*(uhat/sint+aa*fzero)
+            phlt(i) = cfn*cost*uhat*(uhat/sint+RADIUS_OF_EARTH_IN_METERS*fzero)
         end associate
     end do
 
@@ -229,39 +224,41 @@ program test
 
     !  initialize sphere derived data type.
 
-    call initialize_gaussian_spherical_harmonic(this,nlon,nlat,ntrunc,aa)
+    call initialize_gaussian_spherical_harmonic(this,nlon,nlat,ntrunc,RADIUS_OF_EARTH_IN_METERS)
 
     do j=1,nlon
         associate( lambda => real(j - 1, kind=wp) * LONGITUDINAL_MESH )
             cos_l = cos(lambda)
             sin_l = sin(lambda)
         end associate
-        do i = 1, nlat
+        associate( gaulats => this%gaulats )
+            do i = 1, nlat
 
-            !     lambda is longitude, theta is colatitude, and pi/2-theta is
-            !     latitude on the rotated grid. lhat and that are longitude
-            !     and colatitude on the unrotated grid. see text starting at
-            !     equation (3.10)
-            !
-            associate( theta => HALF_PI-asin(this%gaulats(i)) )
-                cos_t = cos(theta)
-                sin_t = sin(theta)
-                sint = cos_a*cos_t+sin_a*sin_t*cos_l
-                cthclh = cos_a*sin_t*cos_l-sin_a*cos_t
-                cthslh = sin_t*sin_l
-                lhat = atanxy(cthclh,cthslh)
-                clh = cos(lhat)
-                slh = sin(lhat)
-                cost = clh*cthclh+slh*cthslh
-                that = atanxy(sint,cost)
-                uhat = compute_initial_unrotated_longitudinal_velocity(uzero,HALF_PI-that)
-                pxact(j,i) = compute_cosine_transform(that,phlt)
-                uxact(j,i) = uhat*(cos_a*sin_l*slh+cos_l*clh)
-                vxact(j,i) = uhat*(cos_a*cos_l*slh*cos_t-clh*sin_l*cos_t+sin_a*slh*sin_t)
-                f(j,i) = fzero*sint
-                coslat(j,i) = sqrt(1.0_wp - this%gaulats(i)**2)
-            end associate
-        end do
+                !     lambda is longitude, theta is colatitude, and pi/2-theta is
+                !     latitude on the rotated grid. lhat and that are longitude
+                !     and colatitude on the unrotated grid. see text starting at
+                !     equation (3.10)
+                !
+                associate( theta => HALF_PI-asin(gaulats(i)) )
+                    cos_t = cos(theta)
+                    sin_t = sin(theta)
+                    sint = cos_a*cos_t+sin_a*sin_t*cos_l
+                    cthclh = cos_a*sin_t*cos_l-sin_a*cos_t
+                    cthslh = sin_t*sin_l
+                    lhat = atanxy(cthclh,cthslh)
+                    cos_lh = cos(lhat)
+                    sin_lh = sin(lhat)
+                    cost = cos_lh*cthclh+sin_lh*cthslh
+                    that = atanxy(sint,cost)
+                    uhat = compute_initial_unrotated_longitudinal_velocity(uzero,HALF_PI-that)
+                    pxact(j,i) = compute_cosine_transform(that,phlt)
+                    uxact(j,i) = uhat*(cos_a*sin_l*sin_lh+cos_l*cos_lh)
+                    vxact(j,i) = uhat*(cos_a*cos_l*sin_lh*cos_t-cos_lh*sin_l*cos_t+sin_a*sin_lh*sin_t)
+                    f(j,i) = fzero * sint
+                    coslat(j,i) = sqrt(1.0_wp - gaulats(i)**2)
+                end associate
+            end do
+        end associate
     end do
 
     vmax = 0.0_wp
@@ -272,8 +269,8 @@ program test
         do i=1,nlon
             v2max = v2max+uxact(i,j)**2+vxact(i,j)**2
             p2max = p2max+pxact(i,j)**2
-            vmax = amax1(abs(uxact(i,j)),abs(vxact(i,j)),vmax)
-            pmax = amax1(abs(pxact(i,j)),pmax)
+            vmax = max(abs(uxact(i,j)),abs(vxact(i,j)),vmax)
+            pmax = max(abs(pxact(i,j)),pmax)
         end do
     end do
     !
@@ -294,9 +291,9 @@ program test
 
     !==> time step loop.
 
-    nnew = 1
-    nnow = 2
-    nold = 3
+    n_new = 1
+    n_now = 2
+    n_old = 3
 
     do cycle_number = 0, MAXIMUM_NUMBER_OF_TIME_ITERATIONS
 
@@ -319,7 +316,7 @@ program test
             v = vg/coslat
             p = pg
             htime = time/3600.0_wp
-            write(stdout,390) cycle_number,htime,dt,nlat,nlon,ntrunc,omega,pzero,uzero,alphad
+            write(stdout,390) cycle_number,htime,dt,nlat,nlon,ntrunc,ROTATION_RATE_OF_EARTH,pzero,uzero,alphad
 
 390         format(//' steady nonlinear rotated flow:',/    &
                 ' cycle number              ',i10,     &
@@ -338,22 +335,30 @@ program test
             dpmax = 0.0_wp
             evmax = 0.0_wp
             epmax = 0.0_wp
+
             do j=1,nlat
                 do i=1,nlon
-                    dvgm = amax1(dvgm,abs(divg(i,j)))
-                    dvmax = dvmax+(u(i,j)-uxact(i,j))**2+(v(i,j)-vxact(i,j))**2
-                    dpmax = dpmax+(p(i,j)-pxact(i,j))**2
+                    dvgm = &
+                        max(dvgm,abs(divg(i,j)))
+                    dvmax = &
+                        dvmax+(u(i,j)-uxact(i,j))**2+(v(i,j)-vxact(i,j))**2
+                    dpmax = &
+                        dpmax+(p(i,j)-pxact(i,j))**2
                     evmax = &
-                        amax1(evmax,abs(v(i,j)-vxact(i,j)),abs(u(i,j)-uxact(i,j)))
-                    epmax = amax1(epmax,abs(p(i,j)-pxact(i,j)))
+                        max(evmax,abs(v(i,j)-vxact(i,j)),abs(u(i,j)-uxact(i,j)))
+                    epmax = &
+                        max(epmax,abs(p(i,j)-pxact(i,j)))
                 end do
             end do
+
             dvmax = sqrt(dvmax/v2max)
             dpmax = sqrt(dpmax/p2max)
             evmax = evmax/vmax
             epmax = epmax/pmax
+
             write(stdout,391) evmax,epmax,dvmax,dpmax,dvgm
-391         format(' max error in velocity',1pe15.6,&
+391         format(&
+                ' max error in velocity',1pe15.6,&
                 ' max error in geopot. ',1pe15.6,/&
                 ' l2 error in velocity ',1pe15.6,&
                 ' l2 error in geopot.  ',1pe15.6,/&
@@ -362,18 +367,28 @@ program test
 
         !==> COMPUTE RIGHT-HAND SIDES OF PROGNOSTIC EQNS.
 
-        scrg1(:,:) = ug(:,:)*(vrtg(:,:)+f(:,:))
-        scrg2(:,:) = vg(:,:)*(vrtg(:,:)+f(:,:))
+        scrg1 = ug * ( vrtg + f )
+        scrg2 = vg * ( vrtg + f )
+
         call perform_multiple_real_fft(this, scrg1, scrm1, 1)
         call perform_multiple_real_fft(this, scrg2, scrm2, 1)
-        call get_complex_spherical_harmonic_coefficients(this,scrm1,scrm2,dvrtdtnm(:,nnew),-1,1)
-        call get_complex_spherical_harmonic_coefficients(this,scrm2,scrm1,ddivdtnm(:,nnew),1,-1)
-        scrg1(:,:) = ug(:,:)*(pg(:,:)+pzero)
-        scrg2(:,:) = vg(:,:)*(pg(:,:)+pzero)
-        call get_complex_spherical_harmonic_coefficients(this,scrm1,scrm2,dpdtnm(:,nnew),-1,1)
-        scrg1(:,:)=pg(:,:)+0.5*((ug(:,:)**2+vg(:,:)**2)/coslat(:,:)**2)
-        call perform_spherical_harmonic_transform(this,scrg1,scrnm,1)
-        ddivdtnm(:,nnew)=ddivdtnm(:,nnew)-this%lap(:)*scrnm(:)
+
+        call get_complex_spherical_harmonic_coefficients(this,scrm1,scrm2,dvrtdtnm(:,n_new),-1,1)
+        call get_complex_spherical_harmonic_coefficients(this,scrm2,scrm1,ddivdtnm(:,n_new),1,-1)
+
+        scrg1 = ug * ( pg + pzero )
+        scrg2 = vg * ( pg + pzero )
+
+        call get_complex_spherical_harmonic_coefficients( &
+            this, scrm1, scrm2, dpdtnm(:,n_new), -1, 1)
+
+        scrg1 = pg + 0.5_wp * ( ( ug**2 + vg**2 ) / coslat**2 )
+
+        call perform_spherical_harmonic_transform( this, scrg1, scrnm, 1)
+
+        associate( lap => this%lap )
+            ddivdtnm(:,n_new) = ddivdtnm(:,n_new) - lap * scrnm
+        end associate
 
         !==> update vrt and div with third-order adams-bashforth.
 
@@ -381,44 +396,49 @@ program test
 
         select case (cycle_number)
             case (0)
-                dvrtdtnm(:,nnow) = dvrtdtnm(:,nnew)
-                dvrtdtnm(:,nold) = dvrtdtnm(:,nnew)
-                ddivdtnm(:,nnow) = ddivdtnm(:,nnew)
-                ddivdtnm(:,nold) = ddivdtnm(:,nnew)
-                dpdtnm(:,nnow) = dpdtnm(:,nnew)
-                dpdtnm(:,nold) = dpdtnm(:,nnew)
+                dvrtdtnm(:,n_now) = dvrtdtnm(:,n_new)
+                dvrtdtnm(:,n_old) = dvrtdtnm(:,n_new)
+                ddivdtnm(:,n_now) = ddivdtnm(:,n_new)
+                ddivdtnm(:,n_old) = ddivdtnm(:,n_new)
+                dpdtnm(:,n_now) = dpdtnm(:,n_new)
+                dpdtnm(:,n_old) = dpdtnm(:,n_new)
             case (1)
-                dvrtdtnm(:,nold) = dvrtdtnm(:,nnew)
-                ddivdtnm(:,nold) = ddivdtnm(:,nnew)
-                dpdtnm(:,nold) = dpdtnm(:,nnew)
+                dvrtdtnm(:,n_old) = dvrtdtnm(:,n_new)
+                ddivdtnm(:,n_old) = ddivdtnm(:,n_new)
+                dpdtnm(:,n_old) = dpdtnm(:,n_new)
         end select
 
-        vrtnm(:) = vrtnm(:) + dt*(&
-            (23.0_wp/12.0_wp)*dvrtdtnm(:,nnew) &
-            - (16.0_wp/12.0_wp)*dvrtdtnm(:,nnow)+&
-            (5.0_wp/12.0_wp)*dvrtdtnm(:,nold) )
-        divnm(:) = divnm(:) + dt*(&
-            (23.0_wp/12.0_wp)*ddivdtnm(:,nnew) &
-            - (16.0_wp/12.0_wp)*ddivdtnm(:,nnow)+&
-            (5.0_wp/12.0_wp)*ddivdtnm(:,nold) )
-        pnm(:) = pnm(:) + dt*(&
-            (23.0_wp/12.0_wp)*dpdtnm(:,nnew) &
-            - (16.0_wp/12.0_wp)*dpdtnm(:,nnow)+&
-            (5.0_wp/12.0_wp)*dpdtnm(:,nold) )
+        vrtnm = &
+            vrtnm + dt * (&
+            (23.0_wp/12.0_wp) * dvrtdtnm(:,n_new) &
+            - (16.0_wp/12.0_wp) * dvrtdtnm(:,n_now) &
+            + (5.0_wp/12.0_wp) * dvrtdtnm(:,n_old) )
+
+        divnm = &
+            divnm + dt *( &
+            (23.0_wp/12.0_wp) * ddivdtnm(:,n_new) &
+            - (16.0_wp/12.0_wp) * ddivdtnm(:,n_now) &
+            + (5.0_wp/12.0_wp) * ddivdtnm(:,n_old) )
+
+        pnm = &
+            pnm + dt * (&
+            (23.0_wp/12.0_wp) * dpdtnm(:,n_new) &
+            - (16.0_wp/12.0_wp) * dpdtnm(:,n_now) &
+            + (5.0_wp/12.0_wp) * dpdtnm(:,n_old) )
 
         !==> SWITCH INDICES.
 
-        nsav1 = nnew
-        nsav2 = nnow
-        nnew = nold
-        nnow = nsav1
-        nold = nsav2
+        nsav1 = n_new
+        nsav2 = n_now
+        n_new = n_old
+        n_now = nsav1
+        n_old = nsav2
 
     !==> end time step loop
 
     end do
 
-    !==> deallocate pointers in sphere object.
+    !==> deallocate arrays in object.
 
     call destroy_gaussian_spherical_harmonic(this)
 
